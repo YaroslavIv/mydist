5eea98c55639c685aea3db77f0a0a8a1b59c59d2
yroslavcr <yroslavcr@gmail.com>
2024-02-02 07:22:23 +0000

    add impl list-post

--- x/example/keeper/query_list_post.go
+++ x/example/keeper/query_list_post.go
@@ -5,7 +5,10 @@ import (
 
 	"example/x/example/types"
 
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -17,8 +20,23 @@ func (k Keeper) ListPost(goCtx context.Context, req *types.QueryListPostRequest)
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.KeyPrefix(types.PostKey))
 
-	return &types.QueryListPostResponse{}, nil
+	var posts []types.Post
+	pageRes, err := query.Paginate(store, req.Pagination, func(key, value []byte) error {
+		var post types.Post
+		if err := k.cdc.Unmarshal(value, &post); err != nil {
+			return err
+		}
+
+		posts = append(posts, post)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryListPostResponse{Post: posts, Pagination: pageRes}, nil
 }
